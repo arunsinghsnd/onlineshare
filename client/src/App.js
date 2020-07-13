2a6bc0e3e26ab6c40cf8dc5dcc7ff00782de647d
arunsnd2017 <arunsinghsnd@gmail.com>
2020-07-13 19:14:47 +0530

    creating routes adding home,signin ,singup, profile pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import NavBar from './components/Navbar';
+import "./App.css";
+import {BrowserRouter, Route} from 'react-router-dom';
+import Home from './components/screens/Home'
+import SignIn from './components/screens/SignIn'
+import Profile from './components/screens/Profile'
+import Signup from './components/screens/Signup'
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <NavBar/>
+      <Route exact path="/">
+        <Home/>
+      </Route>
+      <Route path="/signin">
+        <SignIn/>
+      </Route>
+      <Route path="/signup">
+        <Signup/>
+      </Route>
+      <Route path="/profile">
+        <Profile/>
+      </Route>
+    </BrowserRouter>
+    
   );
 }
 
