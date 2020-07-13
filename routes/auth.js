a2418d008d7fd16078dd6cf86620ff463d2f120b
arunsnd2017 <arunsinghsnd@gmail.com>
2020-07-13 11:12:54 +0530

    creating signin route and accessing the database

--- routes/auth.js
+++ routes/auth.js
@@ -41,5 +41,30 @@ router.post('/signup',(req,res)=>{
   })
 })
 
+router.post('/signin',(req,res)=>{
+    const {email,password} = req.body
+    if(!email || !password){
+      return  res.status(422).json({error:"Please add email or password"})
+    }
+    User.findOne({email:email})
+    .then(savedUser=>{
+        if(!savedUser){
+         return  res.status(422).json({error:"Invalid Email or Password"})
+        }
+        bcrypt.compare(password,savedUser.password)
+        .then(doMatch=>{
+            if(doMatch){
+                res.json({message: "Successfully signed in"})
+            }
+            else{
+                return  res.status(422).json({error:"Invalid Email or Password"})
+            }
+        })
+        .catch(err=>{
+            console.log(err)
+        })
+    })
+})
+
 
 module.exports = router
