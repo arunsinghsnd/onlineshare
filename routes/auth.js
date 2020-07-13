9e27d088c99d8fd68aece9c528cc311163f959da
arunsnd2017 <arunsinghsnd@gmail.com>
2020-07-13 10:44:46 +0530

    adding the username, password, email to the mogoDB atlas database

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const router = express.Router()
+const mongoose = require('mongoose')
+const User = mongoose.model("User")
 
 
 router.get('/',(req,res)=>{
@@ -11,7 +13,28 @@ router.post('/signup',(req,res)=>{
   if(!email || !password || !name){
     return  res.status(422).json({error: "please add all the fields"})
   }
-  res.json({message: "successfully  posted"})
+  User.findOne({email:email})
+  .then((savedUser)=>{
+      if(savedUser){
+          return res.status(422).json({error:"user already exits with that email"})
+      }
+      const user = new User({
+          email,
+          password,
+          name
+      })
+      user.save()
+      .then(user=>{
+          res.json({message: "saved successfully"})
+      })
+      .catch(err=>{
+          console.log(err)
+      })
+  })
+  .catch(err=>{
+      console.log(err)
+  })
 })
+
 
 module.exports = router
