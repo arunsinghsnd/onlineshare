a82da77e05774ca0edbdc33b454d8e1a6484d69b
arunsnd2017 <arunsinghsnd@gmail.com>
2020-07-13 22:37:56 +0530

    creating Home UI

--- client/src/components/screens/Home.js
+++ client/src/components/screens/Home.js
@@ -1,7 +1,48 @@
 import React from "react";
 
 const Home = () => {
-  return <h1>Home</h1>;
+  return(
+    <div className="home">
+      <div className="card home-card">
+        <h5>arun Singh</h5>
+        <div className="card-image">
+          <img src="https://images.unsplash.com/photo-1477346611705-65d1883cee1e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80"/>
+        </div>
+        <div className="card-content">
+        <i class="material-icons" style={{color: "red"}}>favorite</i>
+          <h6>title</h6>
+          <p>This is amazing post</p>
+          <input type="text" placeholder="add a comment"/>
+        </div>
+      </div>
+
+      <div className="card home-card">
+        <h5>arun Singh</h5>
+        <div className="card-image">
+          <img src="https://images.unsplash.com/photo-1477346611705-65d1883cee1e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80"/>
+        </div>
+        <div className="card-content">
+        <i class="material-icons" style={{color: "red"}}>favorite</i>
+          <h6>title</h6>
+          <p>This is amazing post</p>
+          <input type="text" placeholder="add a comment"/>
+        </div>
+      </div>
+
+      <div className="card home-card">
+        <h5>arun Singh</h5>
+        <div className="card-image">
+          <img src="https://images.unsplash.com/photo-1477346611705-65d1883cee1e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80"/>
+        </div>
+        <div className="card-content">
+        <i class="material-icons" style={{color: "red"}}>favorite</i>
+          <h6>title</h6>
+          <p>This is amazing post</p>
+          <input type="text" placeholder="add a comment"/>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
