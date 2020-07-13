8153761c1d3da6973dcb5e8ff2da4af9b7073b1f
arunsnd2017 <arunsinghsnd@gmail.com>
2020-07-13 11:29:19 +0530

    adding JWT

--- key.js
+++ key.js
@@ -1,3 +1,4 @@
 module.exports={
-    MONGOURI:"mongodb+srv://arunsingh:3rqTboTFE4zJTvGn@cluster0.pvldz.mongodb.net/<dbname>?retryWrites=true&w=majority"
+    MONGOURI:"mongodb+srv://arunsingh:3rqTboTFE4zJTvGn@cluster0.pvldz.mongodb.net/<dbname>?retryWrites=true&w=majority",
+    JWT_SECRET:"fjashdufyhfernsuidhu"
 }
